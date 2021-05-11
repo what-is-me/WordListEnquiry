dc4cc48f54ebf435d4bab72ae2f3f53c33808eb1
Leo.Cai <70659394+what-is-me@users.noreply.github.com>
2021-05-11 21:16:26 +0800

    v1.1 源代码

--- 批量查单词.py
+++ 批量查单词.py
@@ -3,7 +3,7 @@
 #     @Date:           2021-04-29 19:00:39
 #     @LastEditors:    what-is-me
 #     @LastEditTime:   2021-04-29 19:00:40
-#     @Description:    一个简单的爬虫程序，用于批量查询单词，有图形界面。
+#     @Description:    一个简单的爬虫程序，用于批量查询单词，有图形界面。v1.1
 #     @Address:        https://github.com/what-is-me/wordlisttranslate/
 # =======================================================================
 
